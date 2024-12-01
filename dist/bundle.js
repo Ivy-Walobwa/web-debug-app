71dccdcddef5fb17cb46ff94e470ab2fcce34825
Ivy-Walobwa <ivywalobwa@gmail.com>
2024-12-01 15:10:43 +0300

    feat: generate source map

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,2 @@
 (()=>{"use strict";let e=0;const t=document.createElement("button");t.innerText="Click me",document.body.appendChild(t),t.addEventListener("click",(()=>{e+=1,console.log(`Counter: ${e} ${{}.total()}`)}))})();
+//# sourceMappingURL=bundle.js.map
